baf2cabd5e43a059b34c62930c3460323d48e3e9
Paulo Pereira <57216738+paulopereira98@users.noreply.github.com>
2021-07-27 01:20:05 +0100

    update license

--- ccs811.h
+++ ccs811.h
@@ -1,3 +1,22 @@
+/*
+	Driver for the ccs811 sensor for stm32.
+	Copyright (C) 2019  Paulo Pereira	(EEIC-UMinho)
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+ * @file ccs811.h
+ * @brief Driver for the ccs811 sensor for stm32.
+ */
+
 #ifndef _CCS811_H_
 #define _CCS811_H_
 
